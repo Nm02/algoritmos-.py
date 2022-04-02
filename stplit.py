c5f411c395b06a988b74fb8987f620f4532b90f6
Nm02 <93084076+Nm02@users.noreply.github.com>
2022-04-02 15:05:47 -0300

    descarto archivos inutiles

--- stplit.py
+++
@@ -1,8 +0,0 @@
-lin="si,no,quizas."
-V=lin.split(",")
-print(V)
-#separar un string por cada coma, y cargar cara frase resultante al vector
-P="hola    "
-p=P.strip()
-print(P)
-#elimina espacios de una palabra
