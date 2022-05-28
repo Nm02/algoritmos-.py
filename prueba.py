9f42e3b8e59fd5ec965359574be052da586294ba
Nm02 <93084076+Nm02@users.noreply.github.com>
2022-05-28 15:06:58 -0300

    pruebo vs

--- prueba.py
+++ prueba.py
@@ -1,1 +1,25 @@
+def existe(archivo):
+    A=archivo
+    archivo=archivo+".txt"
+    try:
+        F=open(archivo,"r")
+        F.close()
+        
+        F=open(archivo,"a")
+        F.write(A+"\n")
+        F.close()
+    except(Exception):
+        F=open(archivo,"w")
+        F.write(A+"\n")
+        F.close()
+
+
+banco1="macro"
+banco2="rio"
+banco3="macro"
+
+existe(banco1)
+existe(banco2)
+existe(banco3)
+
 print("hola")
